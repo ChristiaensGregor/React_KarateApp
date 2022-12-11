987c354e7bb0ddbadd71311de52ee65e42786c8d
ChristiaensGregor <gregorchristiaens@gmail.com>
2022-12-11 17:03:05 +0100

    runall config and video recording

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "aj3a25",
   e2e: {
     baseUrl: "http://localhost:3000/",
     experimentalStudio: true,
+    experimentalRunAllSpecs: true,
     retries: 2,
     setupNodeEvents(on, config) {
       // implement node event listeners here

86a9e8253295c1b64e785ab6e5d6a6cf497f9004
ChristiaensGregor <gregorchristiaens@gmail.com>
2022-12-18 20:40:40 +0100

    homepage  settings button

--- e2e/cypress/pages/HomePage.js
+++ e2e/cypress/pages/HomePage.js
@@ -2,10 +2,10 @@ class HomePage {
   NavigationHome = () => cy.xpath('//*[@data-cy="navigation-home-button"]');
   NavigationLessons = () => cy.xpath('//*[@data-cy="navigation-Lessons-button"]');
   NavigationUsers = () => cy.xpath('//*[@data-cy="navigation-Users-button"]');
-  NavigationSettings;
+  NavigationSettings = () => cy.xpath('//*[@data-cy="navigation-setting-button"]');
   NavigationSettingsTheme = () => cy.get('[data-cy="navigation-setting-Theme-button"]');
   NavigationSettingsLogin = () => cy.get('[data-cy="navigation-setting-Login-button"]');
   NavigationSettingsLogout = () => cy.get('[data-cy="navigation-setting-Logout-button"]');
 }
 
-module.exports = new HomePage();
+module.exports = new HomePage(); //TODO Adjust this, currently it recreates the pageObject every time?

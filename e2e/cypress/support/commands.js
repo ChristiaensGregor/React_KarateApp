eaa0d162d7a92e2f271fc8686c84360feb48a0fd
ChristiaensGregor <gregorchristiaens@gmail.com>
2023-08-06 02:55:33 +0200

    clean up comments in commands

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -2,6 +2,7 @@ const homePage = require("../pages/HomePage");
 const loginPage = require("../pages/LoginPage");
 
 require("@cypress/xpath");
+
 Cypress.Commands.add("login", (email, password) => {
   homePage.NavigationSettings().click();
   homePage.NavigationSettingsLogin().click();
@@ -11,24 +12,3 @@ Cypress.Commands.add("login", (email, password) => {
   loginPage.PasswordField().type(password);
   loginPage.LoginButton().click();
 });
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })

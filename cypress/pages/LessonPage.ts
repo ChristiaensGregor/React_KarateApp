12bb4bd07e9041a8f005419b0f4eb871306d6b74
ChristiaensGregor <gregorchristiaens@gmail.com>
2023-12-31 11:30:32 +0100

    add, delete, particpate tests

--- cypress/pages/LessonPage.ts
+++ cypress/pages/LessonPage.ts
@@ -1,6 +1,15 @@
 class LessonPage {
   ListTitle = "[data-cy='lesson-list-title']";
   LessonCards = "[data-cy='lesson-card']";
+  AddLesson = "[data-cy='lessons-add-lesson-button']";
+
+  AddLessonTitle = "[data-cy='lessons-add-lesson-card-title']";
+  AddLessonType = "[data-cy='lessons-add-lesson-card-lesson-type']";
+  AddLessonTypeStandard = "[data-cy='lessons-add-lesson-card-lesson-type-standard']";
+  AddLessonLocation = "[data-cy='lessons-add-lesson-card-lesson-location']";
+  AddLessonLocationMariaAalter = "[data-cy='lessons-add-lesson-card-lesson-location-mariaaalter']";
+  AddLessonDate = "[data-cy='lessons-add-lesson-card-lesson-date']";
+  AddLessonButton = "[data-cy='lessons-add-lesson-card-lesson-add']";
 }
 const lessonPage = new LessonPage();
 export default lessonPage;
